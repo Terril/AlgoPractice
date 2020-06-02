5dbb5829adb8f955f0fbfc6d7accd941a2506882
Terril <terril.thomas97@gmail.com>
2020-06-02 09:03:40 +0800

    O(n2) computation time for Snapchat problem

--- src/SnapchatTimeTable.java
+++ src/SnapchatTimeTable.java
@@ -1,5 +1,42 @@
 public class SnapchatTimeTable {
 
     public static void main(String args[]) {
+        ClassTimeTable timeTable = new ClassTimeTable(30, 75);
+        ClassTimeTable timeTabl1 = new ClassTimeTable(0, 50);
+        ClassTimeTable timeTable2 = new ClassTimeTable(60, 150);
+//        ClassTimeTable timeTable3 = new ClassTimeTable(45, 80);
+//        ClassTimeTable timeTable4 = new ClassTimeTable(130, 180);
+        ClassTimeTable[] timeTables = new ClassTimeTable[]{timeTable, timeTabl1, timeTable2};
+
+        int rooms = findNumberofRoomsNeeded(timeTables);
+        System.out.println(rooms);
+    }
+
+    private static int findNumberofRoomsNeeded(ClassTimeTable[] timeTables) {
+        int max_rooms = 0;
+        int rooms = 1;
+        for (int v = 0; v < timeTables.length; v++) {
+            for (int u = v + 1; u < timeTables.length; u++) {
+                if (timeTables[u].startTime < timeTables[v].startTime && timeTables[u].endTime < timeTables[v].endTime)
+                    rooms += 1;
+            }
+
+            if (rooms > max_rooms) {
+                max_rooms = rooms;
+            }
+        }
+
+        return max_rooms;
+    }
+
+    static class ClassTimeTable {
+        int startTime;
+        int endTime;
+
+        ClassTimeTable(int startTime, int endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
     }
 }
