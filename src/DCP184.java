ff58ccfef8443aecab6132f791053f5d389f2eed
Terril <terril.thomas97@gmail.com>
2020-10-27 09:25:12 +0800

    DCP problem No. 184

---
+++ src/DCP184.java
@@ -0,0 +1,24 @@
+public class DCP184 {
+    public static void main(String args[]) {
+        int arr[] = {42, 56, 14};
+        System.out.println(findGCD(arr)); //Greatest common denominator
+    }
+
+    private static int findGCD(int arr[]) {
+        int result = 0;
+        for (int j : arr) {
+            result = gcd(j, result);
+            if (result == 1) {
+                return 1;
+            }
+        }
+
+        return result;
+    }
+
+    private static int gcd(int value, int result) {
+        if (value == 0)
+            return result;
+        return gcd(result % value, value);
+    }
+}
