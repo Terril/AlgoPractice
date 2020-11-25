e780888ded0fbf2ae59b6c12f5de95256067cfdc
Terril Thomas <terril.thomas97@gmail.com>
2020-11-25 13:02:00 +0800

    Daily Coding problem 200 .. Handling multiple stabs

--- src/dcp/DCP200.java
+++ src/dcp/DCP200.java
@@ -7,18 +7,24 @@ package dcp;
 //        Compute the smallest set of points that stabs X.
 //        For example, given the intervals [(1, 4), (4, 5), (7, 9), (9, 12)], you should return [4, 9]
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class DCP200 {
     public static void main(String args[]) {
         Interval a = new Interval(1, 4);
-        Interval b = new Interval(4, 6);
+        Interval b = new Interval(4, 5);
         Interval c = new Interval(7, 9);
         Interval d = new Interval(9, 12);
-        Interval e = new Interval(14, 15);
+        Interval e = new Interval(13, 15);
         Interval arr[] = {a, b, c, d, e};
 
-        Interval in = computeSmallestSetOfSTabs(arr);
 
+        Interval in = computeSmallestSetOfSTabs(arr);
         System.out.println(in.start + "," + in.end);
+
     }
 
     static class Interval {
@@ -28,6 +34,7 @@ public class DCP200 {
             start = i;
             end = i1;
         }
+
     }
 
     private static Interval computeSmallestSetOfSTabs(Interval arr[]) {
@@ -35,18 +42,21 @@ public class DCP200 {
         Interval be = arr[0];
         //  int sepVal = 0;
         Interval res = new Interval(0, 0);
+        List<Integer> intervals = new ArrayList<>();
+        intervals.add(0, 1);
         for (int i = 1; i < len; i++) {
             Interval nw = arr[i];
-
             if (be.end != nw.start) {
                 int sepVal = nw.start - be.end;
-                if (sepVal > 0) {
+                if (sepVal > 0 && sepVal >= intervals.get(0)) {
                     res = new Interval(be.start, nw.end);
+                    intervals.add(0, sepVal);
                 }
             }
 
             be = nw;
         }
+
         return res;
     }
 }
