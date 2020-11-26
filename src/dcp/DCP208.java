4d50adace1300d7dc39a0e0c98b4df607adea212
Terril <terril.thomas97@gmail.com>
2020-11-26 22:09:00 +0800

    Daily coding problem 208 .. partition of the linkedList based on the pivot

---
+++ src/dcp/DCP208.java
@@ -0,0 +1,78 @@
+package dcp;
+
+public class DCP208 {
+    public static void main(String args[]) {
+        Node head = newNode(5);
+        head.next = newNode(1);
+        head.next.next = newNode(8);
+        head.next.next.next = newNode(0);
+        head.next.next.next.next = newNode(3);
+
+        int x = 3;
+        head = partition(head, x);
+        printList(head);
+    }
+
+    static class Node {
+        int data;
+        Node next;
+    }
+
+    static Node newNode(int data) {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        return node;
+    }
+
+    static Node partition(Node head, int pivot) {
+
+        Node smallHead = null;
+        Node largeHead = null;
+        Node largeLast = null;
+        Node smallLast = null;
+        while (head != null) {
+            // If current node is equal to x, append it
+            // to the list of x values
+            if (head.data >= pivot) {
+                if (largeHead == null)
+                    largeLast = largeHead = head;
+                else {
+                    largeLast.next = head;
+                    largeLast = head;
+                }
+            } else {
+                if (smallHead == null)
+                    smallLast = smallHead = head;
+                else {
+                    smallLast.next = head;
+                    smallLast = head;
+                }
+            }
+            head = head.next;
+        }
+
+        if(largeLast != null) {
+            largeLast.next = null;
+        }
+
+//        if (smallHead == null)
+//        {
+//            if (largeLast == null)
+//                return largeHead;
+//            smallLast.next = largeHead;
+//            return smallHead;
+//        }
+        smallLast.next = largeHead;
+        return smallHead;
+    }
+
+    private static void printList(Node node) {
+        Node temp = node;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
