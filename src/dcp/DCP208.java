4b4309727b341dccc018cf4fdc40bf10806a2e60
Terril <terril.thomas97@gmail.com>
2020-11-26 22:09:08 +0800

    Daily coding problem 208 .. partition of the linkedList based on the pivot

--- src/dcp/DCP208.java
+++ src/dcp/DCP208.java
@@ -1,5 +1,13 @@
 package dcp;
 
+/*
+*
+* This problem was asked by LinkedIn.
+* Given a linked list of numbers and a pivot k,
+* partition the linked list so that all nodes less than k come before nodes greater than or equal to k.
+* For example, given the linked list 5 -> 1 -> 8 -> 0 -> 3 and k = 3,
+*  the solution could be 1 -> 0 -> 5 -> 8 -> 3.
+* */
 public class DCP208 {
     public static void main(String args[]) {
         Node head = newNode(5);
