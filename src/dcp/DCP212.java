c2d9e4ccf124a9111bdd5c010a1a2bd83962563e
Terril <terril.thomas97@gmail.com>
2020-12-02 08:54:00 +0800

    Daily Coding problem 212.. Finding the Alphabetical seq based on number

--- src/dcp/DCP212.java
+++ src/dcp/DCP212.java
@@ -1,5 +1,12 @@
 package dcp;
+/*
+* This problem was asked by Dropbox.
 
+* Spreadsheets often use this alphabetical encoding for its columns:
+* "A", "B", "C", ..., "AA", "AB", ..., "ZZ", "AAA", "AAB", ....
+* Given a column number, return its alphabetical column id.
+* For example, given 1, return "A". Given 27, return "AA".
+*/
 public class DCP212 {
     public static void main(String args[]) {
         int number = 27;
