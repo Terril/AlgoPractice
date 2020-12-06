c0c0a54bc9fa3e55c78b2c452404d83b6e0867fb
Terril <terril.thomas97@gmail.com>
2020-12-06 09:56:42 +0800

    Daily coding problem .. Connect 4 game

---
+++ src/dcp/DCP219.java
@@ -0,0 +1,15 @@
+package dcp;
+/*
+* This problem was asked by Salesforce.
+*
+* Connect 4 is a game where opponents take turns dropping red or black discs into a 7 x 6 vertically suspended grid.
+* The game ends either when one player creates a line of four consecutive discs of their color (horizontally, vertically, or diagonally),
+* or when there are no more spots left in the grid.
+*
+* Design and implement Connect 4.
+* */
+public class DCP219 {
+    public static void main(String args[]) {
+        // https://ssaurel.medium.com/creating-a-connect-four-game-in-java-f45356f1d6ba
+    }
+}
