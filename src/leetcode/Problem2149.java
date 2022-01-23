5218206c6c58b981c8c666091b082b1cbc2e3403
Terril <terril.thomas97@gmail.com>
2022-01-23 12:16:18 +0800

    Leetcode ... Rearrage array in a format

--- src/leetcode/Problem2149.java
+++ src/leetcode/Problem2149.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
+/*
+*
+* You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+
+You should rearrange the elements of nums such that the modified array follows the given conditions:
+
+Every consecutive pair of integers have opposite signs.
+For all integers with the same sign, the order in which they were present in nums is preserved.
+The rearranged array begins with a positive integer.
+Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+
+
+*
+* */
+
 public class Problem2149 {
     public static void main(String args[]) {
         int[] in1 = {3,1,-2,-5,2,-4};
